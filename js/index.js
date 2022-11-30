fbfae4cc4051b4f3a7d3a7de682653d34e79a83b
Ignacio Silva <ignaciosilvadeleon@gmail.com>
2022-11-30 18:03:45 +0100

    createElements by JS

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const API_URL = "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects-v2";
 
-// Para conseguir los datos utilizamos el metodo fetch. El fetch nos permite descargar esos datos.
-// El fetch nos devuelve una promesa, por lo tanto la debemos resolver para eso utilizamos (async await o then). Utilizaremos async await.
+// Para conseguir los datos de una API utilizamos el metodo fetch. El fetch nos permite descargar dichos datos.
+// El fetch nos devuelve una promesa, por lo tanto la debemos resolver, para eso utilizamos (async await o then). Utilizaremos async await.
 // Para transformar el response a json, utilizamos el metodo .json() el cual asignamos a la constante data. esto nos devuelve una promesa la cual tenemos que resolver (await). Ahora si data va a ser el resultado de la API.
-// Como nos piden los proyectos mas recientes debemos ordenar el array, para eso utilizamos sort.
-// Para tomar los tres primeros elementos del array utilizamos el metodo .slice().
+// Como nos piden los proyectos mas recientes debemos ordenar el array, para eso utilizamos el metodo sort y lo asignamos a la constante sortedProjects.
+// Para tomar los tres primeros elementos del array utilizamos el metodo .slice() y lo asignamos a la constante recentProjects.
 async function fetchProjects() {
     const response = await fetch(API_URL);
     const data = await response.json();
@@ -19,7 +19,26 @@ async function fetchProjects() {
         return 0;
     });
     const recentProjects = sortedProjects.slice(0, 3);
-    console.log(recentProjects);
+
+    // Para poder renderizar los proyectos mas recientes recorreremos el array recentProjects, pues en caso de que haya menos de tres arrays nuestra pagina no será inestable. Para recorrer el array utilizaremos el metodo .forEach().
+    // Previo a esto debemos seleccionar el elemento que contiene a los tres proyectos. 
+
+    const divProyectos = document.querySelector(".projects")
+
+    recentProjects.forEach(function(project) {
+        const div = `
+            <article class="projects-article">
+                <img
+                src="${project.image}"
+                alt="project section image ${project.uuid}"
+                />
+                <h4>${project.name}</h4>
+                <p>${project.content}</p>
+                <a class="learn-more" href="../projects.html?uuid=${project.uuid}">Learn more</a>
+            </article>
+        `
+        divProyectos.innerHTML += div;
+    })
 }
 
 // El evento load nos indica que la pagina a sido totalemente cargada. Por lo tanto aqui dentro es un buen sitio para acceder a APIs porque esta cargado el contenido estatico y ahora cargamos el contenido dinamico.
