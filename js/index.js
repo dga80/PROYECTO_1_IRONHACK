ef6fe81678fbc6c8f6fe2115a5be845196e941d5
dga80 <dani.dga@gmail.com>
2022-11-30 17:32:01 +0100

    burger index&form

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-sort filter y poco mas
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.classList.toggle("active");
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+// how to close it
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
